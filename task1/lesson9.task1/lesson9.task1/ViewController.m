7a5416290c4ea02369c15bc654155bd3d31bac0e
Maksym Savisko <stowyn@gmail.com>
2015-12-28 23:42:29 +0200

    Task #1. Check for game over added

--- task1/lesson9.task1/lesson9.task1/ViewController.m
+++ task1/lesson9.task1/lesson9.task1/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  lesson9.task1
 //
-//  Created by Anton Lookin on 12/24/15.
+//  Created by Maksym Savisko on 12/28/15.
 //  Copyright © 2015 geekub. All rights reserved.
 //
 
@@ -19,14 +19,15 @@
 //Button
 - (IBAction)startButton:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+//Timer
 @property (nonatomic) NSTimer* myTimer;
 
-//Guesture
+//Gesture
 - (IBAction)firstGesture:(UITapGestureRecognizer *)sender;
 - (IBAction)secondGesture:(UITapGestureRecognizer *)sender;
 - (IBAction)thirdGesture:(UISwipeGestureRecognizer *)sender;
 - (IBAction)fourthGesture:(UISwipeGestureRecognizer *)sender;
-
 
 
 @end
@@ -42,6 +43,7 @@
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+
 
 - (IBAction)startButton:(id)sender {
     if ([self.startButton.titleLabel.text isEqual:@"Start!"]) {
@@ -60,25 +62,42 @@
     if ([self.startButton.titleLabel.text isEqual:@"Stop!"]) {
         NSLog(@"Button title is Stop!");
         self.startButton.hidden = YES;
-        NSLog(@"Current timer is %@", self.myTimer);
         [self.myTimer invalidate];
     }
     
 }
 
+- (void) checkForGameOver {
+    if (self.firstSquare.frame.origin.y >= self.view.frame.size.height - 160) {
+        NSLog(@"Square #1 finished");
+        [self.myTimer invalidate];
+    }
+    if (self.secondSquare.frame.origin.y >= self.view.frame.size.height - 160) {
+        NSLog(@"Square #2 finished");
+        [self.myTimer invalidate];
+    }
+    if (self.thirdSquare.frame.origin.y >= self.view.frame.size.height - 160) {
+        NSLog(@"Square #3 finished");
+        [self.myTimer invalidate];
+    }
+    if (self.fourthSquare.frame.origin.y >= self.view.frame.size.height - 160) {
+        NSLog(@"Square #3 finished");
+        [self.myTimer invalidate];
+    }
+}
+
 - (void) moveSquares {
-    self.firstSquare.frame = CGRectMake(self.firstSquare.frame.origin.x, self.firstSquare.frame.origin.y + 5, self.firstSquare.frame.size.width, self.firstSquare.frame.size.height);
+    int firstSquareSpeed = 5;
+    self.firstSquare.frame = CGRectMake(self.firstSquare.frame.origin.x, self.firstSquare.frame.origin.y + firstSquareSpeed, self.firstSquare.frame.size.width, self.firstSquare.frame.size.height);
     self.secondSquare.frame = CGRectMake(self.secondSquare.frame.origin.x, self.secondSquare.frame.origin.y + 4, self.secondSquare.frame.size.width, self.secondSquare.frame.size.height);
     self.thirdSquare.frame = CGRectMake(self.thirdSquare.frame.origin.x, self.thirdSquare.frame.origin.y + 3.2, self.thirdSquare.frame.size.width, self.thirdSquare.frame.size.height);
     self.fourthSquare.frame = CGRectMake(self.fourthSquare.frame.origin.x, self.fourthSquare.frame.origin.y + 2.56, self.fourthSquare.frame.size.width, self.fourthSquare.frame.size.height);
+    [self checkForGameOver];
 }
 
-
 //Gesture
-
 - (IBAction)firstGesture:(UITapGestureRecognizer *)sender {
-    self.firstSquare.frame = CGRectMake(self.firstSquare.frame.origin.x, 15, self.firstSquare.frame.size.width, self.firstSquare.frame.size.height);
-    
+        self.firstSquare.frame = CGRectMake(self.firstSquare.frame.origin.x, 15, self.firstSquare.frame.size.width, self.firstSquare.frame.size.height);
 }
 
 - (IBAction)secondGesture:(UITapGestureRecognizer *)sender {
